0e88d062da91348c71849e20bdfdf7a45deeac82
Aaron Winter <winter.aaron93@gmail.com>
2019-04-26 14:33:52 +0200

    added global Scene.tscn for changing scenes (with fade) and simple TitleScreen.tscn

---
+++ source/global/Scene.gd
@@ -0,0 +1,50 @@
+extends CanvasLayer
+
+signal scene_loading_done
+
+# Scenes
+const TitleScreen := "res://source/interface/menu/Titlescreen.tscn"
+
+var next_scene := ""
+var loader: ResourceInteractiveLoader = null
+
+onready var anim := $AnimationPlayer as AnimationPlayer
+onready var fps_label := $FPS as Label
+onready var loading_bar := $LoadProgress as ProgressBar
+
+func _process(delta):
+	fps_label.text = "%d FPS" % Engine.get_frames_per_second()
+
+	if loader == null:
+		return
+
+	match loader.poll():
+		OK:
+			loading_bar.value = loader.get_stage()
+		ERR_FILE_EOF:
+			emit_signal("scene_loading_done")
+
+func change(scene: String) -> void:
+	next_scene = scene
+	anim.play("fade_out")
+
+func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
+	if anim_name == "fade_out":
+		loader = ResourceLoader.load_interactive(next_scene)
+		print(loader.get_stage_count())
+		loading_bar.max_value = loader.get_stage_count() - 1
+
+		yield(self, "scene_loading_done")
+
+		if not get_tree().change_scene_to(loader.get_resource()) == OK:
+			print("Global: failed to load ", next_scene)
+
+		anim.play("fade_in")
+		next_scene = ""
+		loader = null
+
+func _on_LoadProgress_value_changed(value: float) -> void:
+	if loading_bar.max_value == value:
+		loading_bar.visible = false
+	elif loading_bar.visible == false:
+		loading_bar.visible = true
