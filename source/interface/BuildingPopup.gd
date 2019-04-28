e729342ff46777589e42caf4d0f6b58f584120c5
dalton5000 <marius.stieme@gmail.com>
2019-04-28 12:35:23 +0200

    add beautifier to popup

--- source/interface/BuildingPopup.gd
+++ source/interface/BuildingPopup.gd
@@ -10,11 +10,11 @@ var upkeep_string = "Upkeep: %s$"
 
 func set_building(slug):
 	building = slug
-	$VBoxContainer/Name.text = str(building.building_name)
-	$VBoxContainer/CostLabel.text = cost_string % str(building.cost)
-	$VBoxContainer/IncomeLabel.text = income_string % str(building.income)
-	$VBoxContainer/UpkeepLabel.text = upkeep_string % str(building.upkep)
-	$VBoxContainer/FlavourLabel.text = str(building.flavourtext)
+	$VBoxContainer/Name.text = Helper.beautify_number(building.building_name)
+	$VBoxContainer/CostLabel.text = cost_string  % Helper.beautify_number(building.cost)
+	$VBoxContainer/IncomeLabel.text = income_string % Helper.beautify_number(building.income)
+	$VBoxContainer/UpkeepLabel.text = upkeep_string % Helper.beautify_number(building.upkep)
+	$VBoxContainer/FlavourLabel.text = Helper.beautify_number(building.flavourtext)
 
 func _on_InvestButton_pressed():
 	emit_signal("invest_pressed")
