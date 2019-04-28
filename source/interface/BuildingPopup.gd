613d1634c700ac89162e7094141fc9f8ce02d2f2
dalton5000 <marius.stieme@gmail.com>
2019-04-28 12:32:37 +0200

    add string formating to popup

--- source/interface/BuildingPopup.gd
+++ source/interface/BuildingPopup.gd
@@ -4,12 +4,16 @@ signal invest_pressed
 
 var building setget set_building
 
+var cost_string = "Investment: %s$"
+var income_string = "Max Income: %s$"
+var upkeep_string = "Upkeep: %s$"
+
 func set_building(slug):
 	building = slug
 	$VBoxContainer/Name.text = str(building.building_name)
-	$VBoxContainer/CostLabel.text = str(building.cost)
-	$VBoxContainer/IncomeLabel.text = str(building.income)
-	$VBoxContainer/UpkeepLabel.text = str(building.upkep)
+	$VBoxContainer/CostLabel.text = cost_string % str(building.cost)
+	$VBoxContainer/IncomeLabel.text = income_string % str(building.income)
+	$VBoxContainer/UpkeepLabel.text = upkeep_string % str(building.upkep)
 	$VBoxContainer/FlavourLabel.text = str(building.flavourtext)
 
 func _on_InvestButton_pressed():
