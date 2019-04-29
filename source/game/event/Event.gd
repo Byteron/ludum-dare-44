0a3f245c71a114301df597e7c396af5c8d504bbb
Aaron Winter <winter.aaron93@gmail.com>
2019-04-29 19:51:39 +0200

    event building requirements

--- source/game/event/Event.gd
+++ source/game/event/Event.gd
@@ -9,6 +9,9 @@ export(float, 0.0, 1.0) var probability = 0.1
 export(String) var title = "Event Title"
 export(String, MULTILINE) var description = "An event happened this week!"
 
+export(Array, String) var required_unbuild = []
+export(Array, String) var required_build = []
+
 onready var tick_timer = $TickTimer
 
 func _ready():
@@ -18,10 +21,28 @@ func _ready():
 func start_tick_timer():
 	tick_timer.start()
 
+func _build_requirements_satisfied():
+	var buildings = Global.Game.building_container
+	for building_name in required_build:
+		if not buildings.has_node(building_name):
+			return false
+		if not buildings.get_node(building_name).is_build:
+			return false
+	return true
+
+func _unbuild_requirements_satisfied():
+	var buildings = Global.Game.building_container
+	for building_name in required_build:
+		if not buildings.has_node(building_name):
+			return false
+		if buildings.get_node(building_name).is_build:
+			return false
+	return true
+
 func _execute():
 	print("OVERWRITE")
 
 func _on_TickTimer_timeout() -> void:
 	randomize()
-	if randf() < probability:
+	if randf() < probability and _build_requirements_satisfied() and _unbuild_requirements_satisfied():
 		emit_signal("happened", self)
