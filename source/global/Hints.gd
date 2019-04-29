7690c0e13ab4613a53e47e380c16733959211272
Aaron Winter <winter.aaron93@gmail.com>
2019-04-30 01:57:31 +0200

    another hint

--- source/global/Hints.gd
+++ source/global/Hints.gd
@@ -5,6 +5,7 @@ var cost_per_hint = 1000
 
 var queue
 var hint_list = [
+	"Click on buildings or building sights to view or invest.",
 	"Lumbercamps are a honest business. I can't see what could go wrong.",
 	"Supermarkets need nearby residents to make profit. This should be obvious.",
 	"Some investments may be a bad idea... I can't tell, that's your job.",
@@ -18,17 +19,17 @@ var hint_list = [
 	"Hospitals generate no income, why were they even invented!",
 	"I'm afraid that's all I can tell you.. Do your best!"
 	]
-	
+
 func _ready():
 	queue = hint_list.duplicate()
 
 func get_next_hint():
-		
+
 	var next_hint = queue[0]
 	queue.pop_front()
-	
+
 	if queue.size() == 0:
 		queue = hint_list.duplicate()
 		cost_per_hint = 0
-		
+
 	return(next_hint)
