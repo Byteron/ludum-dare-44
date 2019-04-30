cd03c6c2a5f216fe10c6b8c03373e6254fa6e548
Aaron Winter <winter.aaron93@gmail.com>
2019-04-30 02:23:53 +0200

    more tweaks

--- source/interface/hud/BuildingPopup.gd
+++ source/interface/hud/BuildingPopup.gd
@@ -5,7 +5,6 @@ var building setget _set_building
 
 onready var name_label = $CenterContainer/VBoxContainer/Name
 onready var cost_label = $CenterContainer/VBoxContainer/CostLabel
-onready var revenue_label = $CenterContainer/VBoxContainer/RevenueLabel
 onready var income_label = $CenterContainer/VBoxContainer/IncomeLabel
 onready var upkeep_label = $CenterContainer/VBoxContainer/UpkeepLabel
 onready var flavour_label = $CenterContainer/VBoxContainer/FlavourLabel
@@ -18,8 +17,7 @@ func _set_building(slug):
 	else:
 		cost_label.text = "Investment: %s$" % Helper.beautify_number(building.cost)
 
-	revenue_label.text = "Max Revenue: %s$/m" % Helper.beautify_number(building.revenue_per_minute())
-	income_label.text = "Income: %s$/m" % Helper.beautify_number(building.income_per_minute())
 	upkeep_label.text = "Upkeep: %s$/m" % Helper.beautify_number(building.upkeep_per_minute())
+	income_label.text = "Income: %s$/m" % Helper.beautify_number(building.income_per_minute())
 
 	flavour_label.text = str(building.flavour_text)
